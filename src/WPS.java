f868ccf5788ad398ff40530646beb9e3adba76b9
xun007 <malin.jakobsen@uib.no>
2018-03-05 15:01:52 +0100

    Added source code from slides and preperation for documents

--- src/WPS.java
+++ src/WPS.java
@@ -1,2 +1,22 @@
-public class WPS {
-}
+import java.io.*;
+import java.net.*;
+    class WPS {
+        public static void main(String argv[]) throws Exception
+        {
+            String clientSentence;
+            String capitalizedSentence;
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+
+            while(true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                System.out.println( "Connection received from: " + connectionSocket.getInetAddress().getHostName() );
+
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println( "Data received: " +clientSentence );
+                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+            }
+        }
+    }
