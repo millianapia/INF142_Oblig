00c0d4aad1750808dc891e53e98bc92851a5678f
xun007 <malin.jakobsen@uib.no>
2018-03-05 15:58:13 +0100

    Changed client and WPS - and added todo's

--- src/WPS.java
+++ src/WPS.java
@@ -1,22 +1,37 @@
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
     class WPS {
+
+//Implementér WPS i Java. (Det finnes forskjellige mekanismer for kommunikasjon mellom en klient og en http-server,
+// men dere skal eksplisitt bruke utveksling av http-meldinger over TCP-socketer.
+
         public static void main(String argv[]) throws Exception
         {
-            String clientSentence;
-            String capitalizedSentence;
             ServerSocket welcomeSocket = new ServerSocket(6789);
-
             while(true) {
                 Socket connectionSocket = welcomeSocket.accept();
                 System.out.println( "Connection received from: " + connectionSocket.getInetAddress().getHostName() );
-
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                clientSentence = inFromClient.readLine();
+                String clientSentence = inFromClient.readLine();
                 System.out.println( "Data received: " +clientSentence );
-                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                getHTTP(clientSentence);
+                String capitalizedSentence = clientSentence.toUpperCase() + '\n';
                 outToClient.writeBytes(capitalizedSentence);
             }
         }
+
+    //TODO: Add return statement
+        static void getHTTP(String urlString) throws Exception{
+            Socket sock = new Socket(urlString, 80);
+            InetAddress addr = sock.getInetAddress();
+            System.out.println("Connected to " + addr);
+            System.out.println(addr.getHostName());
+            System.out.println(addr.getHostAddress());
+            sock.close();
+        }
+
     }
--- src/client.java
+++ src/client.java
@@ -1,31 +1,37 @@
 import java.io.*;
 import java.net.*;
 
+//Implementér klienten K i Java. Kommunikasjon mellom K og WPS skal foregå ved hjelp av UDP. K skal skrive ut
+// (på standard utputt) alle innkommende meldinger fra WPS.
+
     class client {
         public static void main(String args[]) throws Exception
         {
+
             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
             DatagramSocket clientSocket = new DatagramSocket();
-
             InetAddress IPAddress = InetAddress.getByName("hostname");
 
+            //Prep for sending and receiving
             byte[] sendData = new byte[1024];
             byte[] receiveData = new byte[1024];
 
+            //TODO: fix so user can write in the address and port themselves
+            //Sending to WPS--------------------------------------------------------------------
             String sentence = inFromUser.readLine();
             sendData = sentence.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-
             clientSocket.send(sendPacket);
 
+
+            //Received from WPS ----------------------------------------------------------------
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
             clientSocket.receive(receivePacket);
-
             String modifiedSentence = new String(receivePacket.getData());
-
             System.out.println("FROM SERVER:" + modifiedSentence);
             clientSocket.close();
         }
+
+
+
     }
