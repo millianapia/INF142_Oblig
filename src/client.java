646613c15e4d464a9b89b478ed3ca1a8613b42cf
xun007 <malin.jakobsen@uib.no>
2018-03-07 11:29:14 +0100

    Client class updated

--- src/client.java
+++ src/client.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-
+import java.util.Scanner;
 //Implementér klienten K i Java. Kommunikasjon mellom K og WPS skal foregå ved hjelp av UDP. K skal skrive ut
 // (på standard utputt) alle innkommende meldinger fra WPS.
 
@@ -8,27 +8,38 @@ import java.net.*;
         public static void main(String args[]) throws Exception
         {
 
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            Scanner inn = new Scanner(System.in);
+            System.out.println("Enter address: ");
+            String address = inn.nextLine();
+            System.out.println("Are you going to send path? (y/n): ");
+            String answer = inn.nextLine();
+            String path = "/";
+            if(answer.equals("y")) {
+                System.out.println("Enter path:");
+                path = inn.nextLine();
+            }
+
+            address = address + path;
+            inn.close();
+            // Creates socket and gets ip from address
             DatagramSocket clientSocket = new DatagramSocket();
-            InetAddress IPAddress = InetAddress.getByName("hostname");
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+
+
 
             //Prep for sending and receiving
-            byte[] sendData = new byte[1024];
+            byte[] sendData = address.getBytes();
             byte[] receiveData = new byte[1024];
 
-            //TODO: fix so user can write in the address and port themselves
-            //Sending to WPS--------------------------------------------------------------------
-            String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress , 9876);
             clientSocket.send(sendPacket);
 
 
             //Received from WPS ----------------------------------------------------------------
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData());
-            System.out.println("FROM SERVER:" + modifiedSentence);
+            String header = new String(receivePacket.getData());
+            System.out.println("FROM SERVER:" + header);
             clientSocket.close();
         }
 
